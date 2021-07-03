97172cf3c5bc054ca600bb23b97c3cc58ba05d51
EC2 Default User <ec2-user@ip-172-31-18-211.ap-northeast-1.compute.internal>
2021-07-03 09:08:47 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,32 @@
 class UsersController < ApplicationController
   
-  def create
+  def new
+    @user = User.new
   end
   
-  def new
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = "Welcome to the Impressions!"
+      redirect_to @user
+    else
+      render 'new'
+    end
   end
   
   def edit
   end
   
   def show
-    @user = User.find_by(id: params[:user_id])
+    @user = User.find(params[:id])
   end
   
   def update
   end
+  
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
